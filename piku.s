17f4a67b0a212cfca2a7ff16cecbbf61c5d71809
gmonteir <gmonteir@calpoly.edu>
2018-04-06 15:14:38 -0700

    Add piku and readme

--- piku.s
+++ piku.s
@@ -7,8 +7,12 @@ main:
     @ keep the stack 8-byte aligned.
     push    {ip, lr}
 
-    @ Load the argument and perform the call. This is like 'printf("...")' in C.
-    ldr     r0, =message
+    @ Load the arguments and perform the calls. This is like 'printf("...")' in C.
+    ldr     r0, =line1
+    bl      printf
+    ldr     r0, =line2
+    bl      printf
+    ldr     r0, =line3
     bl      printf
 
     @ Exit from 'main'. This is like 'return 0' in C.
@@ -20,5 +24,9 @@ main:
     @ --------------------------------
     @ Data for the printf call. The GNU assembler's ".asciz" directive
     @ automatically adds a NULL character termination.
-message:
-    .asciz  "Hello, world.\n"
+line1:
+    .asciz  "Thirty-five dollars\n"
+line2:
+    .asciz  "The only thing that matters...\n"
+line3:
+    .asciz  "Can it mine Bitcoin?\n"
